0435bb75ffda63844ef43ba8816b49ce2249466d
Eduvieira-1 <edu.vieira1@icloud.com>
2024-06-27 06:52:04 -0300

    courses-card-list

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -14,4 +14,5 @@ import {MatDialog} from "@angular/material/dialog";
 })
 export class CoursesCardListComponent {
 
+  courses = input.required<Course[]>();
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, computed, effect } from '@angular/core';
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
 import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
 import { Course } from '../models/course.model';
@@ -20,19 +20,36 @@ import { CoursesService } from '../services/courses.service';
 export class HomeComponent {
 
 
-  courses = signal<Course[]>([]);
+  #courses = signal<Course[]>([]);
 
   coursesService = inject(CoursesService);
 
+  beginnerCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter(courses => courses.category === "BEGINNER")
+  })
+
+  advancedCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter(courses => courses.category === "ADVANCED")
+  })
+
   constructor(){
+
+    effect(() => {
+      console.log('beginner courses: ', this.beginnerCourses());
+      console.log('advanced courses: ', this.advancedCourses());
+
+    })
+
     this.loadCourses()
-    .then(() => console.log('All courses loaded', this.courses()));
+    .then(() => console.log('All courses loaded', this.#courses()));
   }
 
   async loadCourses(){
     try{
       const courses = await this.coursesService.loadAllCourses();
-      this.courses.set(courses);
+      this.#courses.set(courses);
     } catch(err){
       alert('Error loading courses!')
       console.error(err);
